a83c70ffe6b0f8f430a1f079694c3d29d02510d5
Crowxar <crowxar@gmail.com>
2024-07-19 13:42:31 -0700

    Working on implementing sizing and formatting to condiitons and main

--- conditions.py
+++ conditions.py
@@ -1,62 +1,75 @@
-base_template = """# This is a generated script
+base_template_with_regions = """# This is a generated script made with Tkinter App Creator by Crowxar - https://crowxar.github.io/
 #region ===Imports===
-import tkinter as tk
-from tkinter import ttk
+{imports}
 
 #endregion
 
 #region ===Functions===
+...
 
 #endregion
 
 #region ===App Setup===
 app = tk.Tk()
 app.title("{title}")
-{AppSetup}
-# Conditional block 1 end
+{AppSize}
 
 #endregion
 
 #region ===Data/Variables===
+...
 
 #endregion
 
 #region ===Widgets===
+example_label_widget = ttk.Label(app, text="Thanks for using Tkinter App Creator!", font=("Arial", 18))
+example_label_widget.pack(expand=True, fill="both")
 
 #endregion
 
 #region ===Bindings===
+...
 
 #endregion
 
 #region ===Run===
 app.mainloop()
+
 #endregion
 """
 
+base_template_without_regions = """# This is a generated script made with Tkinter App Creator by Crowxar - https://crowxar.github.io/
+{imports}
 
-AppSetup_1A = """
-screen_width = app.winfo_screenwidth()
-screen_height = app.winfo_screenheight()
-app_width = app.winfo_reqwidth()
+app = tk.Tk()
+app.title("{title}")
+{AppSize}
+
+
+example_label_widget = ttk.Label(app, text="Thanks for using Tkinter App Creator!", font=("Arial", 18))
+example_label_widget.pack(expand=True, fill="both")
+
+app.mainloop()"""
+
+AppSetupAuto = """app_width = app.winfo_reqwidth()
 app_height = app.winfo_reqheight()
-x = (screen_width - app_width) // 2
-y = (screen_height - app_height) // 5
-app.geometry(f"+{x}+{y}")
-app.resizable(False, False)
-"""
-
-AppSetup_1B = """
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
-app_width = {app_width}
+pos_x = (screen_width - app_width) // 2
+pos_y = (screen_height - app_height) // 2
+app.geometry(f"+{pos_x}+{pos_y}")"""
+
+AppSetupSized = """app_width = {app_width}
 app_height = {app_height}
-x = (screen_width - app_width) // 2
-y = (screen_height - app_height) // 5
-app.geometry(f"{{app_width}}x{{app_height}}+{{x}}+{{y}}")
-app.resizable(False, False)
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+pos_x = (screen_width - app_width) // 2
+pos_y = (screen_height - app_height) // 2
+app.geometry(f"{{app_width}}x{{app_height}}+{{pos_x}}+{{pos_y}}")"""
 
 
-label = tk.Label(app, text="Hello, tkinter!")
-label.pack()
-"""
+
+
+
+imports = """import tkinter as tk
+from tkinter import ttk"""
--- main.py
+++ main.py
@@ -20,16 +20,18 @@ def load_default_path():
         return ""
 
 
-def save_default_path(path):
-    data = {"default_path": path}
-    with open("tkinterapp.json", "w") as json_file:
-        json.dump(data, json_file)
+def save_default_path():
+    current_path = path_entry.get()
+    if os.path.exists("tkinterapp.json"):
+        with open("tkinterapp.json", "r") as json_file:
+            data = json.load(json_file)
+            default_path = data.get("default_path")
+    else:
+        default_path = None
 
-
-def on_path_change():
-
-    new_path = path_entry.get()
-    save_default_path(new_path)
+    if current_path != default_path:
+        with open("tkinterapp.json", "w") as json_file:
+            json.dump({"default_path": current_path}, json_file)
 
 
 def browse_folder():
@@ -57,50 +59,6 @@ def is_valid_filename(filename):
     return re.match(regex, filename) is not None
 
 
-def write_script():
-    formatted_title = title_var.get().strip().replace(" ", "_").lower()
-    if not is_valid_filename(formatted_title):
-        updatelabel.config(text="Invalid Filename")
-        return
-    else:
-        valid_title = formatted_title
-
-    updated_size = size_val.get().strip().lower()
-    if not size_var.get():
-        # size_var is False
-        size_tuple = is_valid_size(updated_size)  # Validate size
-        if not size_tuple:
-            updatelabel.config(text="Invalid Size")
-            return
-        else:
-            valid_size = size_tuple  # Set the updated_size to the tuple
-    else:
-        # size_var is True
-        valid_size = "default"
-
-    updatelabel.config(text="Completed")
-    on_path_change()
-
-    if valid_size == "default":
-        segment_one = con.AppSetup_1A
-    else:  # if its a tuple
-        segment_one = con.AppSetup_1B.format(
-            app_width=valid_size[0], app_height=valid_size[1]
-        )
-
-    script_content = con.base_template.format(
-        title=formatted_title, AppSetup=segment_one
-    )
-    path = os.path.join(path_var.get(), f"{formatted_title}.py")
-    try:
-        with open(path, "w") as file:
-            file.write(script_content)
-        updatelabel.config(text="Script created successfully!")
-    #       app.destroy()
-    except Exception as e:
-        updatelabel.config(text=f"Error: {e}")
-
-
 def toggle_size():
     if size_var.get():
         size_entry.config(state="disabled")
@@ -115,6 +73,65 @@ def reset_to_default():
     size_val.set("")
     size_entry.config(state="disabled")
     updatelabel.config(text="Reset to defaults")
+
+
+def write_script():
+    # Check if file title is good
+    formatted_title = title_var.get().strip().replace(" ", "_").lower()
+    if not is_valid_filename(formatted_title):
+        updatelabel.config(text="Invalid Filename")
+        return
+    else:
+        valid_title = formatted_title
+
+    # Check if file size is good
+    if not size_var.get():
+        # size_var is False
+        size_tuple = is_valid_size(size_val.get().strip().lower())  # Validate size
+        if not size_tuple:
+            updatelabel.config(text="Invalid Size")
+            return
+        else:
+            valid_size = size_tuple  # Set the updated_size to the tuple
+    else:
+        # size_var is True
+        valid_size = "default"
+
+    if valid_size == "default":
+        app_size_specs = con.AppSetupAuto
+    else:  # if its a tuple
+        app_size_specs = con.AppSetupSized.format(
+            app_width=valid_size[0],
+            app_height=valid_size[1]
+        )
+
+    if regions_var.get(): # if regions is enabled
+        base_template=con.base_template_with_regions
+    else: # if they're not
+        base_template=con.base_template_without_regions
+    
+
+    
+    script_content = base_template.format(
+            imports = con.imports,
+            title = valid_title,
+            AppSize = app_size_specs,
+        )
+        
+
+    # Check if file path is good with file name, then write script to title
+    path = os.path.join(path_var.get(), f"{valid_title}.py")
+    if os.path.exists(path):
+        updatelabel.config(text="File already exists!")
+    else:
+        try: 
+            with open(path, "w") as file:
+                file.write(script_content)
+            updatelabel.config(text="Script created successfully!")
+            save_default_path()
+        #       app.destroy()
+        except Exception as e:
+            updatelabel.config(text=f"Invalid Path {e}")
 
 
 # endregion
@@ -144,6 +161,10 @@ else:
 title_var = tk.StringVar(value="")
 size_var = tk.BooleanVar(value=True)
 size_val = tk.StringVar(value="")
+center_var = tk.BooleanVar(value=True)
+resizeable_var = tk.BooleanVar(value=True)
+regions_var = tk.BooleanVar(value=True)
+forcetop_var = tk.BooleanVar(value=True)
 # endregion
 
 # region ===Widgets===
@@ -151,30 +172,44 @@ label = ttk.Label(app, text="Tkinter App Creator!", font=("Arial", 18))
 label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 path_entry = ttk.Entry(app, width=20, textvariable=path_var)
-path_entry.grid(row=1, column=1, padx=10)
+path_entry.grid(row=1, column=1, sticky='w')
 path_button = tk.Button(app, text="Path Browse", command=browse_folder)
-path_button.grid(row=1, column=0, padx=10, pady=(5, 2))
+path_button.grid(row=1, column=0, padx=(10,0), pady=(5, 2), sticky='w')
 
 title_label = ttk.Label(app, text="Title", font=("Arial", 12))
-title_label.grid(row=2, column=0, padx=10, pady=(5, 2))
+title_label.grid(row=2, column=0, padx=(10,0), pady=(5, 2), sticky='w')
 title_entry = ttk.Entry(app, width=20, textvariable=title_var)
-title_entry.grid(row=2, column=1, padx=10)
+title_entry.grid(row=2, column=1, sticky='w')
 
 size_checkbox = ttk.Checkbutton(
     app, text="Auto Size", variable=size_var, command=toggle_size
 )
-size_checkbox.grid(row=3, column=0, padx=10, pady=(5, 2))
+size_checkbox.grid(row=3, column=0, padx=(10,0), pady=(5, 2), sticky='w')
 size_entry = ttk.Entry(app, state="disabled", textvariable=size_val)
-size_entry.grid(row=3, column=1, padx=10, pady=(5, 2))
+size_entry.grid(row=3, column=1, pady=(5, 2))
 
-updatelabel = ttk.Label(app, text="Click Submit When Done", font=("Arial", 12))
-updatelabel.grid(row=5, column=0, columnspan=2, padx=10, pady=2)
+center_checkbox = ttk.Checkbutton(app, text="Center", variable=center_var)
+center_checkbox.grid(row=6, column=0, padx=(10,0), pady=2, sticky='w')
 
-complete_button = ttk.Button(app, text="Submit", command=write_script)
-complete_button.grid(row=4, column=1, padx=10, pady=10)
+resizeable_checkbox = ttk.Checkbutton(app, text="Resizeable", variable=resizeable_var)
+resizeable_checkbox.grid(row=7, column=0, padx=(10,0), pady=2, sticky='w')
+
+regions_checkbox = ttk.Checkbutton(app, text="Create Regions", variable=regions_var)
+regions_checkbox.grid(row=7, column=0, padx=(10,0), pady=2, sticky='w')
+
+forcetop_checkbox = ttk.Checkbutton(app, text="Resizeable", variable=forcetop_var)
+forcetop_checkbox.grid(row=8, column=0, padx=(10,0), pady=2, sticky='w')
 
 reset_button = ttk.Button(app, text="Reset", command=reset_to_default)
-reset_button.grid(row=4, column=0, padx=10, pady=10)
+reset_button.grid(row=9, column=0, padx=(10,0), pady=10, sticky='w')
+
+complete_button = ttk.Button(app, text="Submit", command=write_script)
+complete_button.grid(row=9, column=1, pady=10, sticky='w')
+
+updatelabel = ttk.Label(app, text="Click Submit When Done", font=("Arial", 12))
+updatelabel.grid(row=10, column=0, columnspan=2, padx=10, pady=2)
+
+
 
 # endregion
 
